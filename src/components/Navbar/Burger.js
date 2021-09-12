011c499d002c14fa74f258acd5ea8ae9d89a1be1
Haltay <joachim.recio@gmail.com>
2021-09-12 15:38:14 +0100

    add Beginning of navbar

---
+++ src/components/Navbar/Burger.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Burger = () => {
+  
+  
+  return (
+    
+  );
+}
+
+
+export default Burger;
--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -1,14 +1,32 @@
 import React from 'react';
 
-import "./Navbar.css"
+import "./Navabar.css";
 
 const Navbar = () => {
-    return(
-        <div className='navbar'>
+  return (
+    <div className="navbar">
+     
+        <nav role="navigation">
+            <div id="menuToggle">
+            
+                <input type="checkbox" />
 
-        </div>
-
-    );
+                <span></span>
+                <span></span>
+                <span></span>    
+            
+                <ul id="menu">
+                    <a href="/"><li>HOME</li></a>
+                    <a href="/"><li>CLOTHIN</li></a>
+                    <a href="/"><li>SKATEBOARDS</li></a>
+                    <a href="/"><li>FOOTWEAR</li></a>
+                    <a href="/"><li>ACCESORIES</li></a>
+                    <a href="/"><li>BLOG</li></a>      
+                </ul>
+            </div>
+        </nav>
+    </div>
+  );
 };
 
 export default Navbar;
