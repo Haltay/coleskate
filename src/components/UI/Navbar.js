03d263e61077d42bf95d05731018017b8729caa1
Haltay <joachim.recio@gmail.com>
2021-09-12 20:51:29 +0100

    Work on navbar

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -1,30 +1,50 @@
-import React from 'react';
+import React from "react";
+
+import MenuLogo from "../../assets/navbar/cole_type.png";
 
 import "./Navabar.css";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-     
-        <nav role="navigation">
-            <div id="menuToggle">
-            
-                <input type="checkbox" />
+      <div id="menuToggle">
+        <input type="checkbox" className="menu-hamburger" />
 
-                <span></span>
-                <span></span>
-                <span></span>    
-            
-                <ul id="menu">
-                    <a href="/"><li>HOME</li></a>
-                    <a href="/"><li>CLOTHIN</li></a>
-                    <a href="/"><li>SKATEBOARDS</li></a>
-                    <a href="/"><li>FOOTWEAR</li></a>
-                    <a href="/"><li>ACCESORIES</li></a>
-                    <a href="/"><li>BLOG</li></a>      
-                </ul>
-            </div>
-        </nav>
+        <span></span>
+        <span></span>
+        <span></span>
+
+        <ul id="menu">
+          <div className="menu-logo">
+            <img
+              src={MenuLogo}
+              className="menu-logo__image"
+              alt="menu-logo-cole-skate"
+            />
+          </div>
+
+          <div className="menu-content">
+            <a href="/">
+              <div>HOME</div>
+            </a>
+            <a href="/">
+              <div>CLOTHING</div>
+            </a>
+            <a href="/">
+              <div>SKATEBOARDS</div>
+            </a>
+            <a href="/">
+              <div>FOOTWEAR</div>
+            </a>
+            <a href="/">
+              <div>ACCESORIES</div>
+            </a>
+            <a href="/">
+              <div>BLOG</div>
+            </a>
+          </div>
+        </ul>
+      </div>
     </div>
   );
 };
