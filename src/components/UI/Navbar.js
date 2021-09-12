745e38803da77f5bb3bbdeee0701480c6d2a2257
Haltay <joachim.recio@gmail.com>
2021-09-12 23:05:27 +0100

    Navbar done but not finish

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import MenuLogo from "../../assets/navbar/cole_type.png";
+
 import NavbarMenu from "../Navbar/NavbarMenu";
 
 import "./Navabar.css";
@@ -7,6 +9,14 @@ import "./Navabar.css";
 const Navbar = () => {
   return (
     <div className="navbar">
+      <div className="menu-logo-left">
+        <img
+          src={MenuLogo}
+          className="menu-logo__image"
+          alt="menu-logo-cole-skate"
+        />
+      </div>
+
       <div id="menuToggle">
         <input type="checkbox" className="menu-hamburger" />
 
